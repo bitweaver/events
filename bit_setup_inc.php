b6073f2b2c4b1a0d93e0abab076a9013fa71075c
Max Kremmel <xing@synapse.plus.com>
2006-01-26 12:26:03 +0000

    use correct contstants for menu registration

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -3,7 +3,7 @@ global $gBitSystem;
 $gBitSystem->registerPackage( 'events', dirname(__FILE__).'/' );
 
 if ($gBitSystem->isPackageActive( 'events' ) ) {
-    $gBitSystem->registerAppMenu( EVENTS_PKG_DIR, 'Events', EVENTS_PKG_URL.'index.php', 'bitpackage:events/menu_events.tpl', 'events' );
+	$gBitSystem->registerAppMenu( EVENTS_PKG_NAME, ucfirst( EVENTS_PKG_DIR ), EVENTS_PKG_URL.'index.php', 'bitpackage:events/menu_events.tpl', EVENTS_PKG_NAME );
 }
 
 ?>
