6ab72347b05b506e9548cd66ed26d3d656886b6e
Lester Caine <lester@lsces.co.uk>
2006-02-18 07:29:12 +0000

    Update registerPackage hash - so installer can run

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,6 +1,10 @@
 <?php
 global $gBitSystem;
-$gBitSystem->registerPackage( 'events', dirname(__FILE__).'/' );
+$registerHash = array(
+	'package_name' => 'events',
+	'package_path' => dirname( __FILE__ ).'/',
+);
+$gBitSystem->registerPackage( $registerHash );
 
 if ($gBitSystem->isPackageActive( 'events' ) ) {
 	$gBitSystem->registerAppMenu( EVENTS_PKG_NAME, ucfirst( EVENTS_PKG_DIR ), EVENTS_PKG_URL.'index.php', 'bitpackage:events/menu_events.tpl', EVENTS_PKG_NAME );
