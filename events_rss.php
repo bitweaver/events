b1caca6f6256fee836ec7b9691e07fc21ab28b1f
Max Kremmel <xing@synapse.plus.com>
2006-05-04 18:43:22 +0000

    fix rss settings and rename config names in accordance to bitweaver standard

--- events_rss.php
+++ events_rss.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_events/events_rss.php,v 1.2 2006/03/01 20:16:06 spiderr Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_events/events_rss.php,v 1.3 2006/05/04 18:43:22 squareing Exp $
  * @package wiki
  * @subpackage functions
  */
@@ -14,9 +14,10 @@ require_once( EVENTS_PKG_PATH."BitEvents.php" );
 
 $gBitSystem->verifyPackage( 'events' );
 $gBitSystem->verifyPackage( 'rss' );
+$gBitSystem->verifyFeature( 'events_rss' );
 
-$rss->title = $gBitSystem->getConfig( 'title_rss_events', $gBitSystem->mPrefs['siteTitle'].' - '.tra( 'Events' ) );
-$rss->description = $gBitSystem->getConfig( 'desc_rss_events', $gBitSystem->mPrefs['siteTitle'].' - '.tra( 'RSS Feed' ) );
+$rss->title = $gBitSystem->getConfig( 'events_rss_title', $gBitSystem->mPrefs['siteTitle'].' - '.tra( 'Events' ) );
+$rss->description = $gBitSystem->getConfig( 'events_rss_description', $gBitSystem->mPrefs['siteTitle'].' - '.tra( 'RSS Feed' ) );
 
 // check if we want to use the cache file
 $cacheFile = TEMP_PKG_PATH.RSS_PKG_NAME.'/'.EVENTS_PKG_NAME.'_'.$version.'.xml';
@@ -28,7 +29,7 @@ $pParamHash['find'] ='';
 //TODO allow proper sort order
 //$pParamHash['sort_mode'] = "event_date_desc";
 $pParamHash['sort_mode'] = "last_modified_desc";
-$max_records = $gBitSystem->getConfig( 'max_rss_events', 10 );
+$max_records = $gBitSystem->getConfig( 'events_rss_max_records', 10 );
 $pParamHash['offset'] = 0;
 $feeds = $events->getList( $pParamHash );
 $feeds = $feeds['data'];
