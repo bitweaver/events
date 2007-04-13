cccf020263db7c81b409482aeb29a9a4b0f3245e
jetskijoe <jetskijoe@users.sourceforge.net>
2007-04-13 14:30:48 +0000

    Code cleanup moved 2 if statements into 1

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -7,14 +7,13 @@ $registerHash = array(
 );
 $gBitSystem->registerPackage( $registerHash );
 
-if( $gBitSystem->isPackageActive( 'events' ) ) {
-	if( $gBitUser->hasPermission( 'bit_p_read_events' ) ) {
+
+if( $gBitSystem->isPackageActive( 'events' ) &&  $gBitUser->hasPermission( 'bit_p_read_events' ) ) {
 	$menuHash = array(
 		'package_name'  => EVENTS_PKG_NAME,
 		'index_url'     => EVENTS_PKG_URL.'index.php',
 		'menu_template' => 'bitpackage:events/menu_events.tpl',
 	);
 	$gBitSystem->registerAppMenu( $menuHash );
-}
 }
 ?>
