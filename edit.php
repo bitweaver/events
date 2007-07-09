b558f321de64443167f1530170c3f942d59ddff2
Max Kremmel <xing@synapse.plus.com>
2007-07-09 22:03:18 +0000

    clean up ajax library calls by only allowing gBitSystem->loadAjax() instead of assigning loadAjax to the templates. please update your files. also the suggest stuff in the group edit page has a pulse again but it's still in a coma - needs a bit more work

--- edit.php
+++ edit.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_events/edit.php,v 1.8 2007/06/22 23:57:59 nickpalmer Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_events/edit.php,v 1.9 2007/07/09 22:03:18 squareing Exp $
  * Copyright (c) 2004 bitweaver Events
  * @package events
  * @subpackage functions
@@ -20,46 +20,45 @@ $gBitSystem->verifyPermission('p_events_edit' );
 require_once(EVENTS_PKG_PATH.'lookup_events_inc.php' );
 
 if (isset($_REQUEST["title"])) {
-    $gContent->mInfo["title"] = $_REQUEST["title"];
+	$gContent->mInfo["title"] = $_REQUEST["title"];
 }
 
 if (isset($_REQUEST["description"])) {
-    $gContent->mInfo["description"] = $_REQUEST["description"];
+	$gContent->mInfo["description"] = $_REQUEST["description"];
 }
 
 if (isset($_REQUEST["event_time"])) {
-    $gContent->mInfo["event_time"] = $_REQUEST["event_time"];
+	$gContent->mInfo["event_time"] = $_REQUEST["event_time"];
 }
-	    
 
 if (isset($_REQUEST["data"])) {
-    $gContent->mInfo["data"] = $_REQUEST["data"];
+	$gContent->mInfo["data"] = $_REQUEST["data"];
 }
 
 // If we are in preview mode then preview it!
 if (isset($_REQUEST["preview"])) {
-    $gBitSmarty->assign('preview', 'y');
-    $gContent->preview($_REQUEST);
+	$gBitSmarty->assign('preview', 'y');
+	$gContent->preview($_REQUEST);
 } else {
-    $gContent->invokeServices( 'content_edit_function' );
+	$gContent->invokeServices( 'content_edit_function' );
 }
 
 // Pro
 // Check if the page has changed
 if (!empty($_REQUEST["save_events"])) {
-    
-    // Check if all Request values are delivered, and if not, set them
-    // to avoid error messages. This can happen if some features are
-    // disabled
-    if ($gContent->store( $_REQUEST ) ) {
-        header("Location: ".$gContent->getDisplayUrl() );
-        die;
-    } else {
-        $gBitSmarty->assign_by_ref('errors', $gContent->mErrors );
-    }
+
+	// Check if all Request values are delivered, and if not, set them
+	// to avoid error messages. This can happen if some features are
+	// disabled
+	if ($gContent->store( $_REQUEST ) ) {
+		header("Location: ".$gContent->getDisplayUrl() );
+die;
+	} else {
+		$gBitSmarty->assign_by_ref('errors', $gContent->mErrors );
+	}
 }
 
-$gBitSmarty->assign( 'loadAjax', true);
+$gBitSystem->loadAjax( 'prototype' );
 
 // Display the template
 $gBitSystem->display('bitpackage:events/edit_events.tpl', tra('Events') );
