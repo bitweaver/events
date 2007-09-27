611009a23ee4fbeb106f677428e4a0cb8c0165d6
Nick Palmer <nick@sluggardy.net>
2007-09-27 20:52:04 +0000

    Fix permission.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_events/index.php,v 1.5 2007/06/22 23:57:59 nickpalmer Exp $
+// $Header: /cvsroot/bitweaver/_bit_events/index.php,v 1.6 2007/09/27 20:52:04 nickpalmer Exp $
 // Copyright (c) 2004 bitweaver Events
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -11,7 +11,7 @@ require_once('../bit_setup_inc.php' );
 $gBitSystem->verifyPackage('events' );
 
 // Now check permissions to access this page
-$gBitSystem->verifyPermission('p_events_read' );
+$gBitSystem->verifyPermission('p_events_view' );
 
 if (isset($_REQUEST['events_id'] ) ) {
 	require_once(EVENTS_PKG_PATH.'lookup_events_inc.php' );
